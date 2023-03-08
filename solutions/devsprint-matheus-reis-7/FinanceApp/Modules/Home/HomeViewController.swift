91f249d1c98c2d8092bfd83c33f78b07bea5e7c9
renato <felicio95.jr@gmail.com>
2023-03-07 21:32:33 -0300

    feat: Implements a Publisher subject to update HomeHeaderView balance

--- solutions/devsprint-matheus-reis-7/FinanceApp/Modules/Home/HomeViewController.swift
+++ solutions/devsprint-matheus-reis-7/FinanceApp/Modules/Home/HomeViewController.swift
@@ -6,19 +6,39 @@
 //
 
 import UIKit
+import RxSwift
+
+// O saldo é exibido. Proposta: exibir o saldo com abordagem reativa
+// é preciso guardar o saldo (BehaviorRelay)?
 
 class HomeViewController: UIViewController {
 
-    lazy var homeView: HomeView = {
+    // MARK: - Reactive properties
+    /// A viewController é responsável pela exibição e controlar fluxos de dados.
+    /// Portanto observables são criados aqui. Então, como boa prática, é necessária
+    /// uma instância de DisposeBag. Assim, ao fim do ciclo de vida dos observables, juntamente
+    /// a viewController, a disposeBag será responsável por efetuar a liberação da memória alocada.
+    private let disposeBag = DisposeBag()
 
+    private let balanceObservable = PublishSubject<Double>()
+
+    private func bindObservables() {
+        balanceObservable.subscribe(onNext: { balance in
+            print("balanceObservable onNext: \(balance)")
+            self.homeView.homeHeaderView.label.text = String(format: "$%.3f", balance)
+        }).disposed(by: disposeBag)
+    }
+
+    lazy var homeView: HomeView = {
         let homeView = HomeView()
         homeView.delegate = self
         return homeView
     }()
 
     override func viewDidLoad() {
-
         navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Profile", style: .plain, target: self, action: #selector(openProfile))
+        bindObservables()
+        balanceObservable.onNext(42.543)
     }
 
     override func loadView() {
